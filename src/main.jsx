4197fbba75df42c19dc5f9a1c0f8daaa4f09a157
cel3ntato <adriano2024celentano@gmail.com>
2024-08-03 03:31:03 +0200

    configure redux, create reducer, connect to app

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
-import ReactDOM from 'react-dom/client';
-import { App } from './components/App';
-import 'modern-normalize';
-import './index.css';
+import ReactDOM from "react-dom/client";
+import { App } from "./components/App";
+import "modern-normalize";
+import "./index.css";
+import { Provider } from "react-redux";
+import { store } from "./redux/store";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </>
 );
