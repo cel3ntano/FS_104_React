bed52fdf53c5a1e232f39e34bad7af8eb7472135
cel3ntano <adriano2024celentano@gmail.com>
2024-08-04 23:25:10 +0200

    todos slice

--- src/redux/todos/slice.js
+++ src/redux/todos/slice.js
@@ -1,13 +1,20 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-  items: [],
+  items: [{ id: 1, todo: "Learn Redux" }],
 };
 
 const slice = createSlice({
   name: "todos",
   initialState,
-  reducers: {},
+  reducers: {
+    deleteTodo: (state, action) => {
+      state.items = state.items.filter(item => item.id !== action.payload);
+    },
+  },
 });
 
+console.log(slice);
+
 export const todosReducer = slice.reducer;
+export const deleteTodo = slice.actions;
