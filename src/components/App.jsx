4203583d7d14db6d3ce533df33ec32843e4c2024
cel3ntano <adriano2024celentano@gmail.com>
2024-08-15 00:18:16 +0200

    Router

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,23 @@
-import Header from './Header/Header';
-import { TodoList } from './TodoList/TodoList';
+import { Route, Routes } from "react-router-dom";
+import Layout from "./TodoList/Layout";
+import Home from "../pages/Home/Home";
+import Login from "../pages/Login/Login";
+import Register from "../pages/Register/Register";
+import Tasks from "../pages/Tasks/Tasks";
+import NotFound from "../pages/NotFound/NotFound";
 
 const App = () => {
   return (
     <>
-      <Header />
-      <TodoList />
+      <Routes>
+        <Route path='/' element={<Layout />}>
+          <Route index element={<Home />}></Route>
+          <Route path='tasks' element={<Tasks />}></Route>
+        </Route>
+        <Route path='/login' element={<Login />}></Route>
+        <Route path='/register' element={<Register />}></Route>
+        <Route path='*' element={<NotFound />}></Route>
+      </Routes>
     </>
   );
 };
