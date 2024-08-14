5eb54adf5995c9f49d0b3abad3c19f309453e065
cel3ntano <adriano2024celentano@gmail.com>
2024-08-15 00:40:30 +0200

    Formik

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,3 +1,30 @@
+import { Field, Form, Formik } from "formik";
+import { Link } from "react-router-dom";
+
 export default function Login() {
-  return <>Login</>;
+  const initialValues = {
+    email: "",
+    password: "",
+  };
+
+  const handleSubmit = (values, options) => {
+    console.log(values);
+
+    options.resetForm();
+  };
+
+  return (
+    <div>
+      <Formik initialValues={initialValues} onSubmit={handleSubmit}>
+        <Form>
+          <Field name='email' placeholder='Enter email' />
+          <Field name='password' type='password' placeholder='Enter password' />
+          <button type='submit'>Login</button>
+          <p>
+            Need a new account? <Link to='/register'>Sign up</Link>
+          </p>
+        </Form>
+      </Formik>
+    </div>
+  );
 }
--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -1,3 +1,32 @@
+import { Field, Form, Formik } from "formik";
+import { Link } from "react-router-dom";
+
 export default function Register() {
-  return <>Register</>;
+  const initialValues = {
+    name: "",
+    email: "",
+    password: "",
+  };
+
+  const handleSubmit = (values, options) => {
+    console.log(values);
+
+    options.resetForm();
+  };
+
+  return (
+    <div>
+      <Formik initialValues={initialValues} onSubmit={handleSubmit}>
+        <Form>
+          <Field name='name' placeholder='Enter name' />
+          <Field name='email' placeholder='Enter email' />
+          <Field name='password' type='password' placeholder='Enter password' />
+          <button type='submit'>Register</button>
+          <p>
+            Already registered? <Link to='/login'>Sign in</Link>
+          </p>
+        </Form>
+      </Formik>
+    </div>
+  );
 }
